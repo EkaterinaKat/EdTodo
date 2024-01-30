b83d155849700bd1d5178c2ebc3ba60028b918e8
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2024-01-30 13:00:54 +0500

    7.7

--- app/src/main/java/com/example/edtodo/view/MainActivity.java
+++ app/src/main/java/com/example/edtodo/view/MainActivity.java
@@ -2,22 +2,19 @@ package com.example.edtodo.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.edtodo.R;
 import com.example.edtodo.db.Database;
-import com.example.edtodo.logic.Note;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout noteBox;
+    private RecyclerView noteRecycleView;
     private FloatingActionButton addNoteButton;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
+
+        noteAdapter = new NoteAdapter();
+        noteRecycleView.setAdapter(noteAdapter);
 
         addNoteButton.setOnClickListener(view -> {
             Intent intent = CreateNoteActivity.newIntent(this);
@@ -39,22 +39,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        noteBox = findViewById(R.id.noteBox);
+        noteRecycleView = findViewById(R.id.noteRecycleView);
         addNoteButton = findViewById(R.id.addNoteButton);
     }
 
     private void showNotes() {
-        noteBox.removeAllViews();
-        for (Note note : Database.getInstance().getNotes()) {
-            showNoteInBox(note);
-        }
-    }
-
-    private void showNoteInBox(Note note) {
-        View view = getLayoutInflater().inflate(R.layout.note_item, noteBox, false);
-        TextView noteTextView = view.findViewById(R.id.noteTextView);
-        noteTextView.setText(note.getTitle());
-        noteTextView.setBackgroundColor(ContextCompat.getColor(this, note.getPriority().getColorResId()));
-        noteBox.addView(view);
+        noteAdapter.setNotes(Database.getInstance().getNotes());
     }
 }
---
+++ app/src/main/java/com/example/edtodo/view/NoteAdapter.java
@@ -0,0 +1,53 @@
+package com.example.edtodo.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.edtodo.R;
+import com.example.edtodo.logic.Note;
+
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+    private List<Note> notes;
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        Note note = notes.get(position);
+        holder.noteTextView.setText(note.getTitle());
+        holder.noteTextView.setBackgroundColor(ContextCompat.getColor(holder.noteTextView.getContext(),
+                note.getPriority().getColorResId()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+        private final TextView noteTextView;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTextView = itemView.findViewById(R.id.noteTextView);
+        }
+    }
+}
