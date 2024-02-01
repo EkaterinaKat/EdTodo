cd07da9c189b5615993f512ec4b605e896ec050f
EkaterinaKat <katyshevtseva_ekaterina@mail.ru>
2024-02-01 15:22:09 +0500

    8.3

--- app/src/main/java/com/example/edtodo/viewmodel/CreateNoteViewModel.java
+++ app/src/main/java/com/example/edtodo/viewmodel/CreateNoteViewModel.java
@@ -10,29 +10,40 @@ import com.example.edtodo.db.NoteDatabase;
 import com.example.edtodo.logic.Note;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class CreateNoteViewModel extends AndroidViewModel {
     private final NoteDatabase database;
     private final MutableLiveData<Boolean> shouldCloseScreen = new MutableLiveData<>();
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public CreateNoteViewModel(@NonNull Application application) {
         super(application);
         database = NoteDatabase.getInstance(application);
     }
 
-    @SuppressWarnings("CheckResult")
     public void saveNote(Note note) {
-        database.noteDao().add(note)
+        Disposable disposable = database.noteDao().add(note)
                 //данная строчка командует чтобы add(note) выполнялся в фоновом потоке
                 .subscribeOn(Schedulers.io())
                 //данная строчка командует чтобы все инструкции указанные ниже выполнялись в главном потоке
                 .observeOn(AndroidSchedulers.mainThread())
                 //если у Completable не вызвать subscribe тогда метод add(note) не будет выполнен
-                .subscribe(() -> shouldCloseScreen.setValue(true));
+                .subscribe(() -> {
+                    shouldCloseScreen.setValue(true);
+                });
+        compositeDisposable.add(disposable);
     }
 
     public MutableLiveData<Boolean> getShouldCloseScreen() {
         return shouldCloseScreen;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
